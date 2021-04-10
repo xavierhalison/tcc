519feca0c435760ab227ed69faaf9e299a6019aa
xavierhalison <halisonpx@gmail.com>
2021-04-10 16:21:22 -0300

    focus bar

--- src/visualizations/bar.js
+++ src/visualizations/bar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Bar({ data, size, color, labels }) {
   const chartArea = size * 0.9;
@@ -18,9 +18,6 @@ export default function Bar({ data, size, color, labels }) {
     return a.length > b.length ? a : b;
   });
 
-  // tamanho (visual) da maior barra
-  // const highestBarHeight = size * 1;
-
   const dataNumberLength = highestBar.toString().length;
   const baseNumber = Math.pow(10, dataNumberLength - 1);
 
@@ -38,20 +35,23 @@ export default function Bar({ data, size, color, labels }) {
     return (k + 1) * gapWidth + k * barWidth;
   };
 
+  const fontStyle = {
+    fontFamily: "monospace",
+    fontSize: "10px",
+  }
+
+  const [focusedBar, setFocusedBar] = useState(null);
+
   const Bars = () => {
 
     const Marks = () => {
-      const style = {
-        fontFamily: "monospace",
-        fontSize: "10px",
-      };
 
       const marks = [];
       for (let index = 0; index < chartBaseValue; index += (chartBaseValue / 10)) {
         const markX = index === 0 ? chartArea : "5";
         marks.push(
           <>
-            <text style={style} x="-10" y={-getBarHeight(index) + 3} textAnchor="end">{index}</text>
+            <text style={fontStyle} x="-10" y={-getBarHeight(index) + 3} textAnchor="end">{index}</text>
             <path d={`M-5 -${getBarHeight(index)} L${markX} -${getBarHeight(index)}`} stroke="black" />
           </>
         )
@@ -59,7 +59,7 @@ export default function Bar({ data, size, color, labels }) {
 
       marks.push(
         <>
-          <text style={style} x="-10" y={-getBarHeight(chartBaseValue) + 3} textAnchor="end">{chartBaseValue}</text>
+          <text style={fontStyle} x="-10" y={-getBarHeight(chartBaseValue) + 3} textAnchor="end">{chartBaseValue}</text>
           <path d={`M-5 -${getBarHeight(chartBaseValue)} L5 -${getBarHeight(chartBaseValue)}`} stroke="black" />
         </>
       );
@@ -67,25 +67,39 @@ export default function Bar({ data, size, color, labels }) {
       return marks;
     }
 
+    const focusBar = (key, e) => {
+      setFocusedBar(key)
+    }
+
     return (
       <>
         <g transform={`translate(${size - chartArea}, ${barsArea + (size - chartArea)})`}>
           <Marks />
           <path d={`M0 0 L0 -${getBarHeight(chartBaseValue)}`} stroke="black" />
-          {Marks}
           {data.map((value, key) => {
             const barHeight = getBarHeight(value);
 
             return (
-              <rect
-                x={getCurrentX(key)}
-                y={`-${barHeight}`}
-                width={barWidth}
-                height={barHeight}
-                fill={color || "black"}
-                id={`bar_${key}`}
-                key={key}
-              />
+              <>
+                <rect
+                  x={getCurrentX(key)}
+                  y={`-${barHeight}`}
+                  width={barWidth}
+                  height={barHeight}
+                  fill={color || "black"}
+                  id={`bar_${key}`}
+                  key={key}
+                  onMouseOver={(e) => focusBar(key, e)}
+                  onMouseOut={() => setFocusedBar(null)}
+                  stroke={focusedBar === key ? "black" : "transparent"}
+                />
+                {focusedBar === key && (
+                  <path d={`M${getCurrentX(key)} -${barHeight} L0 -${barHeight}`} stroke={color} strokeDasharray="2,3" />
+                )}
+                {focusedBar === key && (
+                  <text style={fontStyle} x={`${getCurrentX(key)}`} y={-(barHeight + 5)} textAnchor="start">{value}</text>
+                )}
+              </>
             );
           })}
         </g>
@@ -94,12 +108,6 @@ export default function Bar({ data, size, color, labels }) {
   };
 
   const Labels = () => {
-    const style = {
-      fontFamily: "monospace",
-      fontSize: "10px",
-    };
-
-
     return (
       <g transform={`translate(${size - chartArea}, ${barsArea + (size - chartArea)})`}>
         {labels.map((value, key) => {
@@ -115,7 +123,7 @@ export default function Bar({ data, size, color, labels }) {
                 d={`M${pathStart} ${labelsArea} L${pathEnd} 0`}
                 id={`${key}_path`}
               />
-              <text style={style} x={hypotenuse - 10} y="0" textAnchor="end">
+              <text style={fontStyle} x={hypotenuse - 10} y="0" textAnchor="end">
                 <textPath xlinkHref={`#${key}_path`} key={`${key}_label`}>
                   {value}
                 </textPath>
